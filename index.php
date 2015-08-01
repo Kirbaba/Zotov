419ac3bfda691e1828d67853b419bb0773fd7227
Vladislav <v.kirbaba@gmail.com>
2015-08-01 13:40:55 +0300

    Small corects

--- index.php
+++ index.php
@@ -4,8 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="">
-    <link rel="icon" href="/wp-content/uploads/2015/03/657068.ico" type="image/x-icon" />
-    <link rel="shortcut icon" href="/wp-content/uploads/2015/03/657068.ico" type="image/x-icon" />
+    <link rel="icon" href="/wp-content/themes/Zotov/img/favicon (1).ico" type="image/x-icon" />
+    <link rel="shortcut icon" href="/wp-content/themes/Zotov/img/favicon (1).ico" type="image/x-icon" />
     <script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAaOWKyamSxMTXclSDFmJ2N4Am20PCTD6I&sensor=FALSE">
     </script>
     <title><?php bloginfo('name'); ?> <?php wp_title(); ?></title>
