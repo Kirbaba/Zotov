0ba7a2b39ec63c58b42229ec0ac6b42b79684c91
Vladislav <v.kirbaba@gmail.com>
2015-08-03 12:47:41 +0300

    Single. Done

--- single.php
+++ single.php
@@ -1,3 +1,18 @@
 <? get_header();?>
-	Hello world
+	<section class="single">
+		<div class="contain">
+		<?php while (have_posts()): the_post(); ?>
+		<?php if ( has_post_thumbnail() ): ?>
+		<h1 class="single__name"><?php the_title(); ?></h1>
+			<div class="single__thumb">
+			  <?php the_post_thumbnail('full', array('class'=>'new-img-pr')); ?>
+			</div>
+				<?php  endif;?>			                     
+			
+			<div class="single__text"> 
+				<?php the_content(); ?>
+			</div>
+		<?php endwhile; ?>
+		</div>
+	</section>
 <? get_footer(); ?>
